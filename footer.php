5de1f66a46b91676ed4bed10612a1eb242a1fd16
Minh Duc Nguyen <68837437+zenki1269@users.noreply.github.com>
2022-10-28 05:40:52 +1100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,22 @@
+<div class="footer">
+  <ul class="footer__sitemap">
+    <li class="footer__sitemap-element">
+      Assignment 3
+      <ul class="footer__sub-sitemap">
+        <li class="footer__sub-sitemap-element">
+          <a href="./index.php">View All Course</a>
+        </li>
+        <li class="footer__sub-sitemap-element">
+          <a href="./create.php"> Create Student</a>
+        </li>
+      </ul>
+    </li>
+    <li class="footer__site-map-element">GitHub
+      <ul class="footer__sub-sitemap">
+        <li class="footer__sub-sitemap-element">
+          <a href="https://github.com/rmit-wp-s2-20222/s3766338-a3">Repository</a>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</div>
